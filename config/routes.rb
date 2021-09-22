8e7244f00774aed02e6201544abc912b16c3b594
Erica Winne <erica.winne@powerhrg.com>
2021-09-22 10:25:01 -0400

    good practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create] 
+  #                         ^in post controller we have def index, def new action
 end
